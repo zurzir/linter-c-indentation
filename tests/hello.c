abcfc4f324f4ec76e17031879e1983176dc0217e
lehilton <lehilton@ic.unicamp.br>
2017-12-10 19:55:51 -0200

    back

---
+++ tests/hello.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+
+int main(int a)
+{
+    if (a())
+    if (b()) ;
+    if (a())
+    if (b()) ;
+
+    if (bla()) {
+    if (ble())
+    echo();
+    }
+
+    switch (3) {
+    case 3:
+    dfddd();
+    for (;;) ;
+
+    if (3) {
+    echo();
+    }
+    }
+}
---
+++ tests/teste.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+void funct()
+{
+	if (1 == 3)
+		print("falso");
+	s;
+
+	for (int i = 0; i < 10; i++) {
+		if (dfd)
+		{
+
+		};
+
+	}
+	//dfd
+
+	if (a)
+		b;
+	else if (d)
+		c;
+	df;
+
+
+	print("espaços antes");
+
+	for (int i = 0; i < 3; i++) {
+
+		while(1) {
+			printf("misturado");
+			printf("misturado de novo");
+			printf("so espaços");
+		 	printf("so tabs");
+			printf("atrás");
+			printf("ok")
+			printf("frente");
+		}
+	}
+}
+
+int main() {
+	// comenta início
+	print("não aumenta bloco")
+
+	// comentário ok
+	printf("com");
+
+	if (1)
+		d;
+
+	// comentário não ok
+	printf("dd")
+
+}
