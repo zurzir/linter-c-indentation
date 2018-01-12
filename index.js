ffc91f1e78124d2a01e6e5b49f375a9852924264
lehilton <lehilton@ic.unicamp.br>
2018-01-12 15:32:32 -0200

    corrigindo bug e atualizando readme

--- index.js
+++ index.js
@@ -238,19 +238,21 @@ function checkIndent(filePath, useTabs, tabLength, text) {
                         paren -= 1;
 
                         // this ")" closes the condition
-                        // and the statement is empty
-                        var nt = nextToken(toks,line,i);
-                        var last_cond = cond_last();
-                        // l(line + 1, 'empty_block', cond_num(), last_cond, nt.value);
-                        if (nt !== null
-                             && nt.value == ';'
-                             && ['if', 'while', 'for', 'else', 'switch'].includes(last_cond))
-                        {
-                            var loc = makeLocation(toks,{line,i}, {line,i},filePath);
-                            notes.push(makeNote(loc, 'Bloco vazio',
-                                `O comando "${last_cond}" contém\
-                                um bloco vazio; todos os comandos depois de ";"\
-                                não fazem parte do "${last_cond}" correpondente.`));
+                        if (paren == 0) {
+                            // and the statement is empty
+                            var nt = nextToken(toks,line,i);
+                            var last_cond = cond_last();
+                            // l(line + 1, 'empty_block', cond_num(), last_cond, nt.value);
+                            if (nt !== null
+                                 && nt.value == ';'
+                                 && ['if', 'while', 'for', 'else', 'switch'].includes(last_cond))
+                            {
+                                var loc = makeLocation(toks,{line,i}, {line,i},filePath);
+                                notes.push(makeNote(loc, 'Bloco vazio',
+                                    `O comando "${last_cond}" contém\
+                                    um bloco vazio; todos os comandos depois de ";"\
+                                    não fazem parte do "${last_cond}" correpondente.`));
+                            }
                         }
                     }
                 }
--- tests/nested.c
+++ tests/nested.c
@@ -4,6 +4,28 @@
 
 #include <stdio.h>
 
+if (n % 4)
+    printf("n é múltiplo de 4\n");
+else if (n % 3)
+    printf("n é múltiplo de 3, mas não é de 4.\n");
+else
+    printf("n não é múltiplo de 3 nem de 4.\n");
+
+
+for (p = list; p->value != x ; p = p->prox);
+
+for (i = 0; bit(chave, i) == bit(t->chave, i); i++);
+
+
+void bubblesort(int *v, int l, int r) {
+    int i, j;
+    for (i = l; i < r; i++)
+        for (j = r; j > i; j--)
+            if (v[j] < v[j-1])
+                troca(&v[j-1], &v[j]);
+}
+
+
 int main() {
     for (;;)
     {
